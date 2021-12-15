61d2784d4e1f4550028ce5a8e6188481d03df5d9
vinayaachar <vinayaachar@gmail.com>
2021-12-15 06:40:57 -0800

    Target Sum - 2 pointer problem

---
+++ two_pointers/pair_with_target_sum.py
@@ -0,0 +1,37 @@
+def pair_with_targetsum(arr, target_sum):
+    left, right = 0, len(arr) - 1
+    while(left < right):
+        current_sum = arr[left] + arr[right]
+        if current_sum == target_sum:
+            return [left, right]
+
+        if target_sum > current_sum:
+            left += 1  # we need a pair with a bigger sum
+        else:
+            right -= 1  # we need a pair with a smaller sum
+    return [-1, -1]
+
+
+# second method using hashmaps
+
+def pair_with_targetsumHashMap(arr, target_sum):
+    nums = {}  # to store numbers and their indices
+    for i, num in enumerate(arr):
+        if target_sum - num in nums:
+            return [nums[target_sum - num], i]
+        else:
+            nums[arr[i]] = i
+    return [-1, -1]
+
+
+def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
